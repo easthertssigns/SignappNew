79de4f16d5b4934133389937b560d5dd51147019
Jalal Ahmad <jalal@wearealight.com>
2013-07-11 15:28:25 +0100

    Added production bucket settings for EU

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -21,7 +21,3 @@ end
 #Spree.user_class = "Spree::LegacyUser"
 Spree.user_class = "Refinery::Memberships::Member"
 
-Paperclip.interpolates(:s3_eu_url) do |att, style|
-  "#{att.s3_protocol}://s3-eu-west-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
-end
-
