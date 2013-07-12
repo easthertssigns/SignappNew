0cce21b9454dff064b6cc8cc8de02ae675bf3e90
Jalal Ahmad <jalal@wearealight.com>
2013-07-12 15:02:15 +0100

    Styled Shapes on product page. styled some part of Cart layout

--- app/models/sign_data.rb
+++ app/models/sign_data.rb
@@ -1,6 +1,15 @@
 class SignData < ActiveRecord::Base
   attr_accessible :description, :height, :base_product_id, :price, :name, :shape_id, :show_as_product, :account_id, :width, :sign_data, :sharing_key, :image
-  has_attached_file :image, :styles => {:extra_large => ["300x300", :jpg], :large => ["300x300", :jpg], :medium => ["300x300", :jpg], :thumb => ["100x100", :jpg] },
-                      :storage => :s3,
-                      :s3_credentials => {:access_key_id => "AKIAJSZTTNHJIAPK672A", :secret_access_key => "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", :bucket => "signapp-dev" }
+  has_attached_file :image,
+                    :styles => {
+                        :extra_large => ["300x300", :jpg],
+                        :large => ["300x300", :jpg],
+                        :medium => ["300x300", :jpg],
+                        :thumb => ["100x100", :jpg]
+                    },
+                    :storage => :s3,
+                    :s3_credentials => "#{Rails.root}/config/s3.yml",
+                    :bucket => "signapp-prod",
+                    :url => '/spree/products/:id/:style/:basename.:extension',
+                    :path => ':rails_root/public/spree/products/:id/:style/:basename.:extension'
 end
--- app/models/sign_graphic.rb
+++ app/models/sign_graphic.rb
@@ -1,7 +1,14 @@
 class SignGraphic < ActiveRecord::Base
   attr_accessible :description, :title, :svg_file, :price
-  has_attached_file :svg_file, :styles => { :medium => ["300x300", :jpg], :thumb => ["100x100", :jpg] },
+  has_attached_file :svg_file,
+                    :styles => {
+                        :medium => ["300x300", :jpg],
+                        :thumb => ["100x100", :jpg] },
                     :storage => :s3,
-                    :s3_credentials => {:access_key_id => "AKIAJSZTTNHJIAPK672A", :secret_access_key => "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", :bucket => "signapp-prod" }
+                    :s3_credentials => "#{Rails.root}/config/s3.yml",
+                    :bucket => "signapp-prod",
+                    :url => '/spree/products/:id/:style/:basename.:extension',
+                    :path => ':rails_root/public/spree/products/:id/:style/:basename.:extension'
+
   has_many :sign_graphic_categories
 end
