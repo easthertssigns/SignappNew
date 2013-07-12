ececf2bcb2d30cceeb0e87340ccac539fb3f6102
Jalal Ahmad <jalal@wearealight.com>
2013-07-12 16:57:12 +0100

    Styled Shapes on product page. styled some part of Cart layout, fixing the sign shapes on product page to display on dropdown.

--- app/models/sign_base_shape.rb
+++ app/models/sign_base_shape.rb
@@ -1,5 +1,16 @@
 class SignBaseShape < ActiveRecord::Base
   attr_accessible :description, :name, :svg_file
-  has_attached_file :svg_file, :styles => { :medium => ["300x300", :jpg], :thumb => ["100x100", :jpg] }
+  has_attached_file :svg_file,
+                    :styles => {
+                        :medium => ["300x300", :jpg],
+                        :thumb => ["100x100", :jpg],
+                        :mini => ["20x20", :jpg]
+                    },
+                    :storage => :s3,
+                    :s3_credentials => "#{Rails.root}/config/s3.yml",
+                    :bucket => "signapp-prod",
+                    :url => '/spree/products/:id/:style/:basename.:extension',
+                    :path => ':rails_root/public/spree/products/:id/:style/:basename.:extension'
+
 end
 
