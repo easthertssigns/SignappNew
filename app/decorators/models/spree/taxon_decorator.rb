ebdd619ec5b9374e3e2c2e22962b9a037aec2a06
Jalal Ahmad <jalal@wearealight.com>
2013-11-14 12:11:00 +0000

    Fixed Taxon image Made some text changes and minor css fixes

--- app/decorators/models/spree/taxon_decorator.rb
+++ app/decorators/models/spree/taxon_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Taxon.class_eval do
 has_attached_file :icon,
-                    :styles => {:mini => '32x32>', :normal => '400x400#'},
+                    :styles => {:mini => '32x32>', :normal => '235x235#'},
                     :default_style => :mini,
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
@@ -8,6 +8,6 @@ has_attached_file :icon,
                     :path => ':rails_root/public/spree/taxons/:id/:style/:basename.:extension',
                     :convert_options => {
                         :mini => "-background transparent -gravity center -extent 75x75 -strip",
-                        :normal => "-background transparent -gravity center -extent 400x400 -strip",
+                        :normal => "-background transparent -gravity center -extent 235x235 -strip",
                     }
 end
