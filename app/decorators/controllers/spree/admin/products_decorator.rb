709940424e34e33fb52608068dfc94ba7ac0fb5e
Doug Mills <slipdigital@gmail.com>
2014-04-28 21:03:40 +0100

    Related Products search woring on related products again.

--- app/decorators/controllers/spree/admin/products_decorator.rb
+++ app/decorators/controllers/spree/admin/products_decorator.rb
@@ -43,40 +43,4 @@ Spree::Admin::ProductsController.class_eval do
   def product_not_found
 
   end
-
-  def collection
-    return @collection if @collection.present?
-
-    unless request.xhr?
-      params[:q] ||= {}
-      params[:q][:deleted_at_null] ||= "1"
-
-      params[:q][:s] ||= "name asc"
-
-      @search = super.ransack(params[:q])
-      @collection = @search.result.
-          not_child_product.
-          group_by_products_id.
-          includes([:master, {:variants => [:images, :option_values]}]).
-          page(params[:page]).
-          per(Spree::Config[:admin_products_per_page])
-
-      if params[:q][:s].include?("master_price")
-        # By applying the group in the main query we get an undefined method gsub for Arel::Nodes::Descending
-        # It seems to only work when the price is actually being sorted in the query
-        # To be investigated later.
-        @collection = @collection.group("spree_variants.price")
-      end
-    else
-      includes = [{:variants => [:images,  {:option_values => :option_type}]}, {:master => :images}]
-
-      @collection = super.where(["name #{LIKE} ?", "%#{params[:q]}%"])
-      @collection = @collection.includes(includes).limit(params[:limit] || 10)
-
-      tmp = super.where(["#{Variant.table_name}.sku #{LIKE} ?", "%#{params[:q]}%"])
-      tmp = tmp.includes(:variants_including_master).limit(params[:limit] || 10)
-      @collection.concat(tmp)
-    end
-    @collection
-  end
 end
